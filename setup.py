22e19ec3e15057ac14d1e708bc4b13c2c795c08e
andylamp <andreas.grammenos@gmail.com>
2023-10-09 02:40:37 +0300

    minor tweaks, bumps in hooks

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 from setuptools import find_packages, setup
 
-VERSION: str = "0.0.6"
+VERSION: str = "0.0.7"
 
 # get the current path
 CURRENT_PATH = os.path.dirname(os.path.abspath(__file__))
