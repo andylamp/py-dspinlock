1d00065a8f4c1ed1befea67f6eced0e729da17e5
andylamp <andreas.grammenos@gmail.com>
2023-10-09 18:58:49 +0300

    reference the environment so it actually gets placed correctly

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 from setuptools import find_packages, setup
 
-VERSION: str = "0.0.8"
+VERSION: str = "0.0.9"
 
 # get the current path
 CURRENT_PATH = os.path.dirname(os.path.abspath(__file__))
